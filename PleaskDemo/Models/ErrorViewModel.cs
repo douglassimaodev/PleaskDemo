676a38217373db9b035f3e57bc6db78c301fa89d
douglas <douglas.simao@sgs.com>
2021-02-06 16:18:26 +0000

    Add project files.

---
+++ PleaskDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PleaskDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
